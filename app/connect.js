a90bbfa12cc44fc95839f9e64ef41d316b3f20de
shishkina <shishkina.kate@gmail.com>
2015-10-17 13:29:42 -0400

    drop function

--- app/connect.js
+++ app/connect.js
@@ -3,16 +3,6 @@
 
 
 // $(".rowA > .cell").on("click",function()
-$(".col").on("click",".cell",function(e){
-//this.cell.attr("data-set-value", "")
-  $(this).css("background-color", "red");
-
-
-})
-
-
-
-
 
 
 // for (var i = 0; i < clickCell; i ++){
@@ -36,20 +26,17 @@ var Column = function(){
 
 };
 
-Column.prototype.makeColumns = function(){
+Column.prototype.makeColumn = function(){
   for (var j = 0; j < 6; j++){
      this.colCells.push(new Cell());
+    }
 
-   }
-
-  }
-
-
+  };
 
 var Board = function(){
 this.boardColumns = [];
-
-  }
+this.currentPlayer = 'R';
+  };
 
 Board.prototype.makeBoard = function(c){
 
@@ -60,12 +47,59 @@ Board.prototype.makeBoard = function(c){
 
 
   }
+Board.prototype.render = function(){
+  if (this.currentPlayer === 'R'){
+             this.currentPlayer === 'B';
+           }
+             else if (this.currentPlayer === 'B'){
+               this.currentPlayer === 'R';
+             }
+
+  }
   var c = new Column();
-  c.makeColumns();
+  c.makeColumn();
   var b = new Board();
   b.makeBoard(c);
 
-var drop = function(){
+var drop = function(c){
 
+    for (var i = 0; i <= c.length; i++){
+        b.render();
+        if(c[i].value === null){
+            c[i].value = b.currentPlayer;
+            c[i].playable();
+            console.log(c[i]);
+          console.log( "Cell " + i + " " + c[i].value);
+          return;
+        }
+    }
 
 }
+
+$(".col").on("click",".cell",function(e){
+
+  if(this.parentElement.id === "A"){
+    console.log(this.parentElement.id);
+    drop(b.boardColumns[0].colCells);
+  }
+  if(this.parentElement.id === "B"){
+    console.log(this.parentElement.id);
+    drop(b.boardColumns[1].colCells);
+  }
+  if(this.parentElement.id === "C"){
+    drop(b.boardColumns[2].colCells);
+  }
+  if(this.parentElement.id === "D"){
+    drop(b.boardColumns[3].colCells);
+  }
+  if(this.parentElement.id === "E"){
+    drop(b.boardColumns[4].colCells);
+  }
+  if(this.parentElement.id === "F"){
+    drop(b.boardColumns[5].colCells);
+  }
+  if(this.parentElement.id === "G"){
+    drop(b.boardColumns[6].colCells);
+  }
+
+});
