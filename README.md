1f2ef123472eddc04175fb0840b6183688b95880
shishkina <shishkina.kate@gmail.com>
2015-10-18 17:42:54 -0400

    CheckWin function done

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Project1_-GA
-Connect4
+##Connect4
+
+I'm trying to build a simple connect 4 game.
+There are two players: red and black. In order to win the game the player needs to connect 4 pieces of the same color either horizontally, vertically, or diagonally.
+
+I set up the grid in a way that I have a Board object which consist of Column objects, inside the Columns, there are Cell objects. At the beginning of the game makeBoard function sets up the Board of empty Column arrays. Every column is clickable, however, only the bottom most cell gets filled up with a value respectively (On every click the player toggles.)
+
+checkWin function is broken up in parts:
+checkHorizontally();
+checkVertically();
+checkDiagonally();
--- app/connect.js
+++ app/connect.js
@@ -1,7 +1,6 @@
   $(function(){("loaded!")});
 
-
-
+// test
 // $(".rowA > .cell").on("click",function()
 
 
@@ -11,27 +10,36 @@
 //   })
 // }
 
-var Cell = function() {
-  this.value = null;
+var Cell = function(value) {
+  this.value = value;
 
   };
-
 Cell.prototype.playable = function(){
   return (this.value)? false : true;
 };
-
 
 var Column = function(){
   this.colCells = [];
 
 };
 
-Column.prototype.makeColumn = function(){
+Column.prototype.makeColumns = function(){
   for (var j = 0; j < 6; j++){
-     this.colCells.push(new Cell());
+     this.colCells.push(new Cell(""));
     }
 
   };
+
+var Row = function(){
+  this.rowCells = [];
+};
+Row.prototype.makeRows = function(b){
+  for (var i = 0; i < b.boardColumns.length; i++){
+        this.rowCells[i] = b.boardColumns[i].colCells[0];
+
+    }
+
+}
 
 var Board = function(){
 this.boardColumns = [];
@@ -49,23 +57,37 @@ Board.prototype.makeBoard = function(c){
   }
 Board.prototype.render = function(){
   if (this.currentPlayer === 'R'){
-             this.currentPlayer === 'B';
+             this.currentPlayer = 'B';
            }
              else if (this.currentPlayer === 'B'){
-               this.currentPlayer === 'R';
+               this.currentPlayer = 'R';
              }
 
   }
+
   var c = new Column();
-  c.makeColumn();
+  c.makeColumns();
   var b = new Board();
   b.makeBoard(c);
 
-var drop = function(c){
+  var r = new Row();
+  r.makeRows(b);
 
+
+// var drop = function(e){
+//     b.render();
+//     if (e.length < 6 ) {
+//     e.push(new Cell(b.currentPlayer));
+//     console.log(e);
+//
+//     return;
+//   }
+//
+// }
+    var drop = function(c){
+      b.render();
     for (var i = 0; i <= c.length; i++){
-        b.render();
-        if(c[i].value === null){
+        if(c[i].value === ""){
             c[i].value = b.currentPlayer;
             c[i].playable();
             console.log(c[i]);
@@ -75,31 +97,210 @@ var drop = function(c){
     }
 
 }
+// var drop = function(boardColumns){
+//
+//   for (var i = boardColumns.length-1 ; i >=0; i--){
+//       console.log(boardColumns.eq([i]));
+//       if(boardColumns.eq([i]).text("")){
+//             boardColumns.eq([i]).text("R");
+//             boardColumns
+//       }
+//       return;
+//     }
+//   }
 
-$(".col").on("click",".cell",function(e){
+// var checkWin(playedCell, boardColumns, x ) {
+//
+//
+// }
+// horizontal win logic:
 
-  if(this.parentElement.id === "A"){
+ var horizontalWin = function(i,j){
+
+   if (b.boardColumns[i-1].colCells[j] != null && b.boardColumns[i-1].colCells[j].value === b.boardColumns[i].colCells[j].value){
+
+      if(b.boardColumns[i-2].colCells[j] != null && b.boardColumns[i-2].colCells[j].value === b.boardColumns[i].colCells[j].value){
+
+        if( b.boardColumns[i-3].colCells[j] != null && b.boardColumns[i-3].colCells[j].value === b.boardColumns[i].colCells[j].value){
+          return true;
+        }
+        else if( b.boardColumns[i+1].colCells[j] != null && b.boardColumns[i+1].colCells[j].value === b.boardColumns[i].colCells[j].value){
+          return true;
+        }
+         else return false;
+       }
+
+        else if(b.boardColumns[i+1].colCells[j] != null && b.boardColumns[i+1].colCells[j].value === b.boardColumns[i].colCells[j].value){
+
+            if(b.boardColumns[i+2].colCells[j] != null && b.boardColumns[i+2].colCells[j].value === b.boardColumns[i].colCells[j].value){
+              return true;
+            }
+            else return false;
+        }
+        else return false;
+
+   }
+
+
+   else if(b.boardColumns[i+1].colCells[j] != null && b.boardColumns[i+1].colCells[j].value === b.boardColumns[i].colCells[j].value){
+
+      if( b.boardColumns[i+2].colCells[j] != null && b.boardColumns[i+2].colCells[j].value === b.boardColumns[i].colCells[j].value){
+
+        if(b.boardColumns[i+3].colCells[j] !== null && b.boardColumns[i+3].colCells[j].value === b.boardColumns[i].colCells[j].value){
+          return true;
+        }
+        return false;
+      }
+      return false;
+   }
+   else return false;
+
+}
+
+var verticalWin = function(i,j){
+
+  if (b.boardColumns[i].colCells[j-1] != null && b.boardColumns[i].colCells[j-1].value === b.boardColumns[i].colCells[j].value){
+
+     if(b.boardColumns[i].colCells[j-2] != null && b.boardColumns[i].colCells[j-2].value === b.boardColumns[i].colCells[j].value){
+
+       if( b.boardColumns[i].colCells[j-3] != null && b.boardColumns[i].colCells[j-3].value === b.boardColumns[i].colCells[j].value){
+         return true;
+       }
+       else if( b.boardColumns[i].colCells[j+1] != null && b.boardColumns[i].colCells[j+1].value === b.boardColumns[i].colCells[j].value){
+         return true;
+       }
+        else return false;
+      }
+
+       else if(b.boardColumns[i].colCells[j+1] != null && b.boardColumns[i].colCells[j+1].value === b.boardColumns[i].colCells[j].value){
+
+           if(b.boardColumns[i].colCells[j+2] != null && b.boardColumns[i].colCells[j+2].value === b.boardColumns[i].colCells[j].value){
+             return true;
+           }
+           else return false;
+       }
+       else return false;
+
+  }
+
+  else if(b.boardColumns[i].colCells[j+1] != null && b.boardColumns[i].colCells[j+1].value === b.boardColumns[i].colCells[j].value){
+
+     if( b.boardColumns[i].colCells[j+2] != null && b.boardColumns[i].colCells[j+2].value === b.boardColumns[i].colCells[j].value){
+
+       if(b.boardColumns[i].colCells[j+3] !== null && b.boardColumns[i].colCells[j+3].value === b.boardColumns[i].colCells[j].value){
+         return true;
+       }
+       return false;
+     }
+     return false;
+  }
+  else return false;
+}
+
+var diagonalWin = function(i,j) {
+  if (b.boardColumns[i-1].colCells[j-1] != null && b.boardColumns[i-1].colCells[j-1].value === b.boardColumns[i].colCells[j].value){
+
+     if(b.boardColumns[i-2].colCells[j-2] != null && b.boardColumns[i-2].colCells[j-2].value === b.boardColumns[i].colCells[j].value){
+
+       if( b.boardColumns[i-3].colCells[j-3] != null && b.boardColumns[i-3].colCells[j-3].value === b.boardColumns[i].colCells[j].value){
+         return true;
+       }
+       else if( b.boardColumns[i+1].colCells[j+1] != null && b.boardColumns[i+1].colCells[j+1].value === b.boardColumns[i].colCells[j].value){
+         return true;
+       }
+        else return false;
+      }
+
+       else if(b.boardColumns[i+1].colCells[j+1] != null && b.boardColumns[i+1].colCells[j+1].value === b.boardColumns[i].colCells[j].value){
+
+           if(b.boardColumns[i+2].colCells[j+2] != null && b.boardColumns[i+2].colCells[j+2].value === b.boardColumns[i].colCells[j].value){
+             return true;
+           }
+           else return false;
+       }
+       else return false;
+
+  }
+
+  else if(b.boardColumns[i+1].colCells[j+1] != null && b.boardColumns[i+1].colCells[j+1].value === b.boardColumns[i].colCells[j].value){
+
+     if( b.boardColumns[i+2].colCells[j+2] != null && b.boardColumns[i+2].colCells[j+2].value === b.boardColumns[i].colCells[j].value){
+
+       if(b.boardColumns[i+3].colCells[j+3] !== null && b.boardColumns[i+3].colCells[j+3].value === b.boardColumns[i].colCells[j].value){
+         return true;
+       }
+       return false;
+     }
+     return false;
+  }
+  else return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+// console.log($("#A > div").eq(1).text("R"));
+
+
+// $(".col").on("click",".cell",function(e){
+//      if(this.parentElement.id === "A"){
+//     console.log(this.parentElement.id);
+//     drop($(this.parentElement).children());
+//     this.parentElement.length -1;
+//   }
+//
+//
+// });
+
+//   $(".col").on("click",".cell",function(e){
+//   if(this.parentElement.id === "B"){
+//     console.log(this.parentElement.id);
+//     var boardColumns = b.boardColumns[1].colCells;
+//     for (var i = 0; i <= boardColumns.length; i++){
+//       if (boardColumns[i].value === null){
+//         boardColumns[i].value = "R";
+//         boardColumns[i].playable();
+//         console.log(boardColumns[i]);
+//              console.log( "Cell " + i + " " + boardColumns[i].value);
+//              return;
+//       }
+//
+//     }
+//   }
+//
+//
+// });
+
+$("#A").on("click",".cell",function(e){
     console.log(this.parentElement.id);
     drop(b.boardColumns[0].colCells);
-  }
-  if(this.parentElement.id === "B"){
+  });
+$("#B").on("click",".cell",function(e){
     console.log(this.parentElement.id);
     drop(b.boardColumns[1].colCells);
-  }
-  if(this.parentElement.id === "C"){
-    drop(b.boardColumns[2].colCells);
-  }
-  if(this.parentElement.id === "D"){
-    drop(b.boardColumns[3].colCells);
-  }
-  if(this.parentElement.id === "E"){
-    drop(b.boardColumns[4].colCells);
-  }
-  if(this.parentElement.id === "F"){
-    drop(b.boardColumns[5].colCells);
-  }
-  if(this.parentElement.id === "G"){
-    drop(b.boardColumns[6].colCells);
-  }
+  });
 
-});
+//   if(this.parentElement.id === "C"){
+//     drop(b.boardColumns[2].colCells);
+//   }
+//   if(this.parentElement.id === "D"){
+//     drop(b.boardColumns[3].colCells);
+//   }
+//   if(this.parentElement.id === "E"){
+//     drop(b.boardColumns[4].colCells);
+//   }
+//   if(this.parentElement.id === "F"){
+//     drop(b.boardColumns[5].colCells);
+//   }
+//   if(this.parentElement.id === "G"){
+//     drop(b.boardColumns[6].colcolCellss);
+//   }
+//
+// });
