31cfa2ee8654d5f0f08b4812777a0ba0533e2cb1
shishkina <shishkina.kate@gmail.com>
2016-06-17 12:00:56 -0400

    cleaned up comments, finalized diagonal logic

--- public/app/app.js
+++ public/app/app.js
@@ -1,67 +1,97 @@
 $( document ).ready(function(){
+console.log("loaded");
 
-    var
-    start = function(){
+var start = function(){
     	    count = 0;
           gameOver = false;
           togglePlayer();
-    },
+    };
     //clear the board on the newGame button click
-    newGame = function(message){
+var newGame = function(message){
         if (confirm(message)){
             start();
             forAllCells(function(i,j){
             	$("#c-" + i + "-" + j).removeClass();
             });
         }
-    },
+    };
 
-    forAllCells = function(action){
-      //the loops start iteration with 1, so the cells are assigned values startin with 1
-      //column assignment
+var forAllCells = function(action){
+      //the loops start iteration with 1, so the cells are assigned values starting with 1
+      //row assignment starts with the top row being first, and the bottow one - 6th
         for (var i = 1; i < 7; i++){
-          //row assignment
+        //assign columns
             for (var j = 1; j < 8; j++){
               //cell assignment
                 action(i,j);
             }
         }
-    },
+    };
     // function to check if the cell has the same class as current player
     // to use in checkWin
-    sameColor = function(i,j){
+var sameColor = function(i,j){
         return $("#c-" + i + "-" + j).hasClass(players[current]);;
-    },
-    togglePlayer = function(){
+    };
+var togglePlayer = function(){
         $("#c").html(players[current = (current + 1) % 2]);
-    },
-    horizontalWin = function(i, j){
+    };
+var horizontalWin = function(i, j){
       //check lefthand first
       for (var left = j-1; left >= 1; left--){
         if(!sameColor(i, left))break;
         //then righthand
           for(var right = j+1; right <= 7; right++){
-            if(!sameColor(i, right)) break;
+            if(!sameColor(i, right))break;
           }
       }
-            return right - left > 4;
+      return right - left > 4;
 
-    },
-    verticalWin = function(i, j){
-      for(var down  = i+1; down <= 6; down++){
+    };
+var verticalWin = function(i, j){
+      for(var down = i+1; down <= 6; down++){
         if(!sameColor(down, j))break;
       }
         return down - i > 3;
-    },
-    // yet to add the winning logic
-    // to handle horizontal/vertical/diagonal wins
-    addCellBehavior = function(i,j){
-        $("#c-" + i + "-" + j).click(function(){
+    };
+    // handle diagonal wins
+var diagonalLeftUpWin = function(i,j){
+      /*add temporary variables to access the diagonal cells
+      up, down variables correspond to the diagonal i(row),
+      temp variable holds the value of the diagonal j(column),
+      as it loops through, first,
+      make sure that loop does not go ouside of the boundaries of the board,
+      then check if  the color of the diagonal cell is the same, if not, BREAK.
+      */
+      var up, down, temp;
+      for(up = i-1, temp = j-1; up > 0; up--, temp--) {
+        if( temp < 1 || !sameColor(up,temp))break;
+          }
+
+      for(down = i+1, temp = j+1; down < 7; down++,temp++){
+        if(temp > 7 || !sameColor(down,temp))break;
+          }
+          //return true or false
+        return down - up > 4;
+    };
+var diagonalRightUpWin = function(i,j){
+  //same principle as diagonalLeftUpWin
+  var up, down, temp;
+        for(up = i-1, temp = j+1; up > 0; up--, temp++) {
+          if(temp > 7 || !sameColor(up, temp))break;
+          }
+        for(down = i+1, temp = j-1; down < 7; down++, temp--) {
+          if(temp < 1 || !sameColor(down, temp))break;
+
+          }
+        return down - up > 4;
+    };
+var addCellBehavior = function(t,j){
+        $("#c-" + t + "-" + j).click(function(){
                 if(!gameOver){
                     for (var t = 6; t >= 1; t--){
                         if($("#c-" + t + "-" + j).hasClass('')){
                             $("#c-" + t + "-" + j).addClass(players[current]);
-                            if(horizontalWin(t, j) || verticalWin(t, j)){
+                            if(horizontalWin(t, j) || verticalWin(t, j) || diagonalLeftUpWin(t,j) || diagonalRightUpWin(t,j)){
                               gameOver = true;
                                 newGame(winMessage.replace("%s",players[current]));
                             } else{
@@ -75,12 +105,12 @@ $( document ).ready(function(){
                             }
                             break;
                         }
-                    }
+                      }
                 }
         });
-    },
+    };
     //initialization of the board
-        init = function(){
+var init = function(){
       var table = $('<table></table>').attr({id:"gameboard"});
     		for (var i = 1; i < 7; i++) {
     			var row = $('<tr></tr>').appendTo(table);
@@ -89,22 +119,19 @@ $( document ).ready(function(){
     			}
     		}
     		table.appendTo("#game");
-    },
+    };
     //counter for counting the moves in order to handle a tie
-    count,
-    players = [$("#red").html(),$("#black").html()],
-    current = 0,
-    newGameMessage = $("#n").html(),
-    winMessage = $("#win").html(),
-    gameOver;
-    init();
-    start();
-    forAllCells(addCellBehavior);
-    $("#new").click(function(){
-        newGame(newGameMessage);
-    });
+var count;
+var players = [$("#red").html(),$("#black").html()];
+var current = 0;
+var newGameMessage = $("#n").html();
+var winMessage = $("#win").html();
+var gameOver;
+init();
+start();
+forAllCells(addCellBehavior);
+$("#new").click(function(){
+  newGame(newGameMessage);
+  });
 
-    window.test={
-    	players:players,
-    }
 });
